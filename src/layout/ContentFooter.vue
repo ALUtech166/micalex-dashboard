b88b703e0fa5183f6a7202cdbc4e2582029141e2
ALUtech166 <s.tassiga@alustudent.com>
2022-08-11 10:21:59 +0000

    new-cmmit

---
+++ src/layout/ContentFooter.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer class="footer">
+    <div class="container-fluid">
+      <nav>
+        <ul class="footer-menu">
+          <li>
+            <router-link :to="{path:'/admin'}">Dashboard</router-link>
+          </li>
+        </ul>
+      </nav>
+      <div class="copyright text-center">
+        Designed by <a href="https://www.mekengroup.com" target="_blank">MEG</a>.
+      </div>
+    </div>
+  </footer>
+</template>
+<script>
+  export default {}
+
+</script>
+<style>
+
+</style>
